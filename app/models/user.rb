1dfbc6c794c949004a6891612f0890845a372558
Deeksha-Kalia123 <dkaliaom1993@gmail.com>
2018-05-10 14:33:23 +0530

    10 may commit via cmdline

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :playlists, dependent: :destroy
   has_many :songs, through: :playlists
   has_many :photos, through: :playlists
+  before_action :authenticate_user!
 end
